773c01face0ac3ac2e42b47c82202d5279ba565a
codemarz <133914851+codemarz@users.noreply.github.com>
2024-02-12 19:11:30 -0500

    Add files via upload

---
+++ Project-_Recipe_App-Marzea_Mukarrama-Solution (1)/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./App.css";
+import RecipeCreate from "./RecipeCreate";
+import RecipeList from "./RecipeList";
+import RecipeData from "./RecipeData"
+
+function App() {
+  const [recipes, setRecipes] = useState(RecipeData);
+
+    const addRecipe = (recipeToAdd) => setRecipes([ ...recipes, recipeToAdd]);
+    const deleteRecipe = (recipeToDelete) => {
+    setRecipes((currentRecipes) => currentRecipes.filter((recipe, index) => index !== recipeToDelete))
+  }
+
+  
+  return (
+    <div className="App">
+      <header><h1>Delicious Food Recipes</h1></header>
+      <RecipeList recipes={recipes} deleteRecipe = {deleteRecipe} />
+      <RecipeCreate addRecipe = {addRecipe} />
+    </div>
+  );
+}
+
+export default App;
